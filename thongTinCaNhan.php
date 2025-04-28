45a45fc705d93ab2629c554809347603ada8b8b6
Trân <habaotran1003205@gmail.com>
2025-04-28 13:58:57 +0700

    Add files via upload

---
+++ thongTinCaNhan.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Thông tin sinh viên</title>
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/thongtinsinhvien.css" />
+</head>
+<body>
+  <?php include("header.php"); ?>
+
+  <div class="content">
+    <?php include("sidebar.php"); ?>
+
+    <div class="content__main">
+      <header>
+        <h1>Thông tin sinh viên</h1>
+      </header>
+
+      <div class="cc">
+        <section class="ttsv">
+          <h2>Thông tin sinh viên</h2>
+          <p>Họ và tên:</p>
+          <p>Ngày sinh:</p>
+          <p>Giới tính:</p>
+          <p>Số điện thoại:</p>
+          <p>Dân tộc:</p>
+          <p>Số CCCD:</p>
+          <p>Mã sinh viên:</p>
+          <p>Email:</p>
+          <p>Tôn giáo:</p>
+          <p>Đối tượng:</p>
+          <p>Chức vụ Đoàn:</p>
+          <p>Ngày vào Đoàn:</p>
+          <p>Chức vụ Đảng:</p>
+          <p>Ngày vào Đảng:</p>
+          <p>Tình trạng học:</p>
+          <p>Tỉnh/Thành phố:</p>
+          <p>Quận/Huyện:</p>
+          <p>Quốc gia:</p>
+          <p>Địa chỉ thường trú:</p>
+          <p>Trình độ học vấn:</p>
+          <p>Trình độ ngôn ngữ:</p>
+        </section>
+
+        <div class="right">
+          <section class="ttkh">
+            <h2>Thông tin khoá học</h2>
+            <p>Ngành học:</p>
+            <p>Niên khóa:</p>
+            <p>LH đào tạo:</p>
+            <p>Lớp sinh viên:</p>
+            <p>Chức vụ:</p>
+            <p>LH đào tạo (CNHT):</p>
+            <p>Cố vấn học tập:</p>
+            <p>LH CVHT:</p>
+            <p>Tài khoản ngân hàng:</p>
+            <p>Tên ngân hàng:</p>
+            <p>STK ngân hàng:</p>
+            <p>Email tài khoản ngân hàng:</p>
+          </section>
+          <div class="hi">
+          <aside>
+            <h2>Liên hệ người thân</h2>
+            <p>Họ và tên:</p>
+            <p>Quan hệ:</p>
+            <p>Số điện thoại:</p>
+            <p>Địa chỉ thường trú:</p>
+          </aside>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <?php include("footer.php"); ?>
+</body>
+</html>
